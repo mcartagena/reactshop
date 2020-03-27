612c0072f4ca9f45947f1857b9aa7049c3ae50a6
mcartagena <marcelocartagenaorellana@gmail.com>
2020-03-27 18:36:00 -0400

    Project initialitation

---
+++ src/Product.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Product: React.SFC<{}> = props => {
+  return <React.Fragment>TODO</React.Fragment>;
+};
+
+export default Product;
---
+++ src/Routes.tsx
@@ -0,0 +1,71 @@
+import { Suspense } from "react";
+
+import * as React from "react";
+
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  RouteComponentProps,
+  Switch
+} from "react-router-dom";
+
+import ProductsPage from "./ProductsPage";
+
+import Header from "./Header";
+
+import ProductPage from "./ProductPage";
+
+import NotFoundPage from "./NotFoundPage";
+
+import LoginPage from "./LoginPage";
+
+const AdminPage = React.lazy(() => import("./AdminPage"));
+
+const RoutesWrap: React.SFC = () => {
+  return (
+    <Router>
+      <Route component={Routes} />
+    </Router>
+  );
+};
+
+const Routes: React.SFC<RouteComponentProps> = props => {
+  const [loggedIn, setLoggedIn] = React.useState(true);
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition
+          key={props.location.key}
+          timeout={500}
+          classNames="animate"
+        >
+          <Switch>
+            <Redirect exact={true} from="/" to="/products" />
+            <Route exact={true} path="/products" component={ProductsPage} />
+            <Route path="/products/:id" component={ProductPage} />
+
+            <Route path="/admin">
+              {loggedIn ? (
+                <Suspense
+                  fallback={<div className="page-container">Loading...</div>}
+                >
+                  <AdminPage />
+                </Suspense>
+              ) : (
+                <Redirect to="/login" />
+              )}
+            </Route>
+            <Route path="/login" component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default RoutesWrap;
